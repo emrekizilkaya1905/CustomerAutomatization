ddaafd0cb9f1d08749223d8e55f22451718a67f9
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-08-15 20:07:28 +0200

    Tested connection

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            dataGridView1 = new DataGridView();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Controls.Add(dataGridView1);
+            panel1.Location = new Point(12, 27);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(776, 285);
+            panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(776, 285);
+            dataGridView1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel2.Location = new Point(12, 318);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(776, 142);
+            panel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 477);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private DataGridView dataGridView1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,46 @@
+using System.Data;
+using System.Data.SqlClient;
+
 namespace CustomerAutomatization
 {
     public partial class Form1 : Form
     {
+        SqlConnection connection = new SqlConnection(@"Data Source=EMRE\SQLEXPRESS;Initial Catalog=ProjectsVT;Integrated Security=True;Encrypt=False");
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            showData();
+        }
+
+        private void showData()
+        {
+            {
+                try
+                {
+                    string sqlSentence = "SELECT CustomerId, Name, Surname, Salary, EligibleForCredit, City FROM Customer";
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlSentence, connection);
+                    DataTable dt = new DataTable();
+                    dataAdapter.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+                catch (Exception ex) {
+                    MessageBox.Show("There has been error occured." + ex.Message);
+                }
+                finally
+                {
+                    if (connection != null)
+                    {
+                        connection.Close();
+                    }
+                }
+            }
         }
     }
 }
